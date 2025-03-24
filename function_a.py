c5a1cf44e93953f66266710fc6ba6205a63f4a4c
Laura Castro <lauracastrotech@gmail.com>
2025-03-24 17:28:08 -0400

    change something

--- function_a.py
+++ function_a.py
@@ -7,3 +7,6 @@ def most_common_value(number_list):
 if __name__ == "__main__":
     nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3]
     print(f"Most common value = {most_common_value(nums)}")
+
+
+print("I hope that this causes a merge conflict!")
